dd444da91a4ead86e5d8c74a53807d0f610c3df6
alijnmerchant21 <alijnmerchant@live.com>
2023-05-02 21:05:00 +0100

    Add support for slack channel

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ import tweepy
 import time
 import config
 import datetime
+import requests
+import json
 
 # Twitter API credentials
 auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
@@ -19,12 +21,16 @@ except Exception as e:
 search_query = "Tendermint"
 last_tweet_id = None
 
-# Define the function to search for tweets
-def search_tweets():
+# Define the Slack webhook URL and channel
+slack_webhook_url = "https://hooks.slack.com/services/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"
+slack_channel = "#general"
+
+# Define the function to search for tweets and send a Slack message
+def search_tweets_and_send_slack_message():
     global last_tweet_id
     # Search for recent tweets with the search query
     if last_tweet_id is not None:
-        tweets = api.search_tweets(q=search_query, count=100, result_type="recent", max_id=last_tweet_id)
+        tweets = api.search_tweets(q=search_query, count=100, result_type="recent", since_id=last_tweet_id)
     else:
         tweets = api.search_tweets(q=search_query, count=100, result_type="recent")
     for tweet in tweets:
@@ -34,11 +40,18 @@ def search_tweets():
         print(f"Found a tweet that mentions {search_query} at {datetime.datetime.now()}:")
         print(tweet_text)
         print(tweet_url)
-        if last_tweet_id is None or tweet.id < last_tweet_id:
+        if last_tweet_id is None or tweet.id > last_tweet_id:
             last_tweet_id = tweet.id
+        # Send a Slack message
+        slack_message = {
+            "channel": slack_channel,
+            "text": f"A tweet that mentions {search_query} was found:\n{tweet_text}\n{tweet_url}"
+        }
+        response = requests.post(slack_webhook_url, data=json.dumps(slack_message), headers={"Content-Type": "application/json"})
+        if response.status_code != 200:
+            print(f"Failed to send Slack message: {response.text}")
 
 # Run the search function every 15 minutes
 while True:
-    search_tweets()
+    search_tweets_and_send_slack_message()
     time.sleep(900)
-    print("The bot is now on snooze")
